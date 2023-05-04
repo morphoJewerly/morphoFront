5a9ed63436fa8153a7ea7fcd4f8812a3fb4178c2
Greg Ivanchuk <gregiv99gmail.com>
2023-05-04 17:30:14 +0300

    hnjhbnjn

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL : "https://140e-188-191-234-130.eu.ngrok.io"
+    baseURL : "https://140e-188-191-234-130.eu.ngrok.io",
+    withCredentials: true,
+    crossdomain: true,
+   
 })
 instance.interceptors.request.use((config) => {
 config.headers.Authorization = window.localStorage.getItem("token");
