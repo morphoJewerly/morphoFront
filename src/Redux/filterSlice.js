4601514005f17aaee9b3ef4db53027d6e1436fe1
GregIvanchuk <gregiv99@gmail.com>
2023-07-25 18:13:41 +0300

    jn

--- src/Redux/filterSlice.js
+++ src/Redux/filterSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 export const initialState ={
-    catId:1,
-    selected:1,
+    catId:0,
+    selected:0,
 }
 export const filterSlice = createSlice({
   name: "filter",
