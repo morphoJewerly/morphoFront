4e402ddc4ffdddb4d156496a33a384fca03d5d7a
arturysh <artur2002236@gmail.com>
2025-03-21 01:24:55 +0200

    fix some bugs

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import styles from './Footer.module.css';
 import {Helmet} from "react-helmet";
 import {Link, useLocation} from "react-router-dom";
+import React from "react";
 // import Accordion from '../Accordion';
 function Footer() {
   const location = useLocation();
@@ -16,10 +17,15 @@ function Footer() {
        
         <div className={styles.rigthFooter}>
           <div className={styles.socialPages}>
-            <a href="https://www.facebook.com/The.piece.of.sky" target="_blank"><img width={42} height={40} src="/images/facebook.png" alt=""  /></a>
-            <a href="https://www.instagram.com/morpho_jewelry_/" target="_blank"><img width={33} height={33} src="/images/instagram.png"  alt=""  /></a>
-            <a href="https://www.tiktok.com/@morpho_jewelry" target="_blank"><img width={35} height={37} src="/images/tiktok.png"  alt=""  /></a>
+            <a href="https://www.facebook.com/The.piece.of.sky" target="_blank"><img width={35} height={35} src="/images/facebook.svg" alt=""  /></a>
+            <a href="https://www.instagram.com/morpho_jewelry_/" target="_blank"><img width={35} height={35} src="/images/instagram.svg"  alt=""  /></a>
+            <a href="https://www.tiktok.com/@morpho_jewelry" target="_blank"><img width={35} height={35} src="/images/tiktok.svg"  alt=""  /></a>
           </div>
+            <div className={styles.footer_nav}>
+                <Link to={"/oferta"}>Угода користувача</Link>
+                <Link to={"/deliverinfo"}>Інформація про доставку</Link>
+                <Link to={"/turnaround"}>Повернення товару</Link>
+            </div>
           <a>morpho.jewelry</a>
           </div>
              {/* <Accordion/> */}
--- src/components/Goods/index.js
+++ src/components/Goods/index.js
@@ -21,6 +21,7 @@ function Goods ({}) {
     const goods = useSelector((state) => state.goods.goods)
     const [isClicked, setIsClicked] =  React.useState(true);
     const [hideInfo, sethideInfo] =  React.useState(false);
+    const [isOpenBurgerMenu, setIsOpenBurgerMenu] = React.useState(false);
     const [idf,setIdf] =  React.useState(0);
      const selected = useSelector((state) => state.filter.selected)
      const count = useSelector((state) => state.cart.totalCount)
@@ -31,25 +32,42 @@ function Goods ({}) {
         }, 1000)
       );
     }, []);
-  
+
     const pullOut = (idt) =>{
       setIdf(idt);
     }
     const ar = [1,2,3,4,5,6,7,8]
 return(
     <>
-    { isClicked ?  
+    { isClicked ?
       <main className={styles.main}>
          <div className={styles.head}>
         {/* <div className={styles.header}>
         <h1 className={styles.h1}>MO<span>R</span>PH<span>O</span></h1>
         <h4 className={styles.h4}>THE PIECE <span>O</span>F SKY</h4>
       </div> */}
+      <div className={`${styles.burger_menu} ${isOpenBurgerMenu ? styles.open : ""}`}>
+          <div className={styles.burger_menu_container}>
+              <Categor categories={categories} catId={catId} OnclickCatIndex={(id) => dispatch(getCatId(id))}/>
+              <Link to={"/oferta"}>Угода користувача</Link>
+              <Link to={"/deliverinfo"}>Інформація про доставку</Link>
+              <Link to={"/turnaround"}>Повернення товару</Link>
+          </div>
+      </div>
       <div className={styles.icons}>
-      <Link to="/about"> <img width={25} height={25} className={styles.home} src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqRoWspnZm3dBSrLQaSlkuK5X1u13UpfUjdA&usqp=CAU"} alt="home" /></Link>
-      { hideInfo ? 
+      <span onClick={() => setIsOpenBurgerMenu(!isOpenBurgerMenu)}>
+          {!isOpenBurgerMenu ? <div className={styles.burger}>
+              <div />
+              <div />
+              <div />
+          </div> :
+          <svg fill="#000000" width="23px" height="23px" viewBox="0 0 16 16" className={styles.cross}>
+              <path d="M0 14.545L1.455 16 8 9.455 14.545 16 16 14.545 9.455 8 16 1.455 14.545 0 8 6.545 1.455 0 0 1.455 6.545 8z" fillRule="evenodd"/>
+          </svg>}
+      </span>
+      { !hideInfo ?
       <p className={styles.p}>THE PIECE OF SKY</p>
-      : 
+      :
       <div className={styles.info}>
       <Accordion/>
       <img onClick={() => {sethideInfo(true)}} className={styles.remimg} width={15} height={15} src="/images/rmv.svg" alt="cart" />
@@ -57,20 +75,20 @@ return(
        }
       <Link to="/cart"> <div className={styles.cart}>
       <img width={35} height={35} src="/images/ct.png" alt="cart" />
-        <span className={styles.badge}>{count}</span> 
-    </div></Link>  
+        <span className={styles.badge}>{count}</span>
+    </div></Link>
          </div>
-     
+
       </div>
       <div className={styles.categorios}>
-       <Categor categories ={categories} catId={catId} OnclickCatIndex={(id) => dispatch(getCatId(id))}/>
+       {/*<Categor categories={categories} catId={catId} OnclickCatIndex={(id) => dispatch(getCatId(id))}/>*/}
        </div>
     <div className={styles.Items1}>
       {isLoading ? ar.map(( _ , index) => <MyLoader key={index}/>):
-      (catId == 0) ? 
+      (catId == 0) ?
       goods.map((item,index) => <Good f1={isClicked} setF1={setIsClicked} callback={(idt)=> pullOut(idt)}  isLoading={isLoading} key={index} {...item}/>):
       goods.filter((obj => (obj.category === catId)))
-    .map((item,index)=> <Good f1={isClicked} setF1={setIsClicked} callback={(idt)=> pullOut(idt)}  isLoading={isLoading} key={index} {...item}/>)} 
+    .map((item,index)=> <Good f1={isClicked} setF1={setIsClicked} callback={(idt)=> pullOut(idt)}  isLoading={isLoading} key={index} {...item}/>)}
       </div>
       <Footer/>
       </main>:
